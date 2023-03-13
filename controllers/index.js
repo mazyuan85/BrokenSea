5579d4ad840b5e46915d63a52d1d8766c9a726e6
mazyuan85 <mazyuan@gmail.com>
2023-03-13 13:20:41 +0800

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,7 @@ const indexPage = async (req, res) => {
         const user = await User.findById(userId).populate("wallets");
         const activeWalletId = req.cookies.activeWallet;
         const activeWallet = await user.wallets.find(w => w.id.toString() === activeWalletId);
-        await res.render('index', { title: 'Welcome To BrokenSea', activeWallet, allCollections });
+        await res.render('index', { title: 'Welcome To BrokenSea', activeWallet, allCollections, userId: req.session.userId });
     } else {
         await res.render('index', { title: 'Welcome To BrokenSea', allCollections });
     }
