d9e3f98b51c58c41d20e6b74eb9f0e279da3c9f0
mazyuan85 <mazyuan@gmail.com>
2023-03-13 13:10:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ app.use(cookieParser());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours in milliseconds
+  }
 }));
 app.use(methodOverride('_method'));
 app.use((req, res, next) => {
